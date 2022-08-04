1c6b581c7fc3621c2c3af4527145bbe886baab3f
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-04 17:35:29 -0300

    dat

--- proyecto_final/nutrisur/models.py
+++ proyecto_final/nutrisur/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class healthdrink(models.Model):
-    nombre = models.CharField(max_length=50, null=True,blank=True)
-    descripcion = models.TextField(max_length=500,null=True,blank=True)
-    precio = models.FloatField(null=True,blank=True)
-    country = models.Text(max_length=500,null=True,blank=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    price = models.FloatField(null=True,blank=True)
+    country = models.Text(max_length=500)
--- proyecto_final/proyecto_final/urls.py
+++ proyecto_final/proyecto_final/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-
-from proyecto_final.nutrisur.models import healthdrink
+from nutrisur.models import healthdrink
 
 urlpatterns = [
     path('admin/', admin.site.urls),
