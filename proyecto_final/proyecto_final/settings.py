4e0254d37192969189e821cc5d1d1724398ac3b3
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-03 23:47:00 -0300

    config de settings

--- proyecto_final/proyecto_final/settings.py
+++ proyecto_final/proyecto_final/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'nutrisur'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'proyecto_final.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
