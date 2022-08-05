8c9b7572e905b725da8d82e2f306e2d73d3f5de8
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-04 21:31:46 -0300

    error en textfield

--- proyecto_final/nutrisur/models.py
+++ proyecto_final/nutrisur/models.py
@@ -4,4 +4,4 @@ class healthdrink(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     price = models.FloatField(null=True,blank=True)
-    country = models.Text(max_length=500)
+    country = models.TextField(max_length=500)
