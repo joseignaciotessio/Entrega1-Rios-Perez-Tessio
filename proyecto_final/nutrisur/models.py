a60a932b8fca836b80af3be49f76c7da7fdf71bd
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-05 17:18:25 -0300

    ok

--- proyecto_final/nutrisur/models.py
+++ proyecto_final/nutrisur/models.py
@@ -5,7 +5,6 @@ class Healthdrink(models.Model):
     description = models.TextField(max_length=500)
     price = models.FloatField(null=True,blank=True)
     country = models.TextField(max_length=500)
-    creation_date = models.DateField(auto_now_add=True, null=True, blank=True)
     stock = models.IntegerField()
     
 class Category(models.Model):
