abba6b0141f6dc6c0426a1249edfb4a03ce79ab6
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-05 21:18:51 -0300

    reparacion

--- proyecto_final/proyecto_final/urls.py
+++ proyecto_final/proyecto_final/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path,include    
+from django.urls import path,include   
+ 
 from nutrisur.views import Healthdrink, Category, Container
 
 urlpatterns = [
