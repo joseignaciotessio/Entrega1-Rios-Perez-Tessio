4fe746b3528c55bce0a1427c5dfb06b25b71380e
Jose Ignacio Tessio <joseignaciotessiocoderhouse@gmail.com>
2022-08-31 23:04:20 -0300

    Pull y push cambios

--- proyecto_final/nutrisur/views.py
+++ proyecto_final/nutrisur/views.py
@@ -152,7 +152,7 @@ def update_product(request, pk):
 
     elif request.method == 'GET':
         product = Healthdrink.objects.get(id=pk)
-        form = Products_upload_form(initial={'name':product.name,'price':product.price,'description':product.description,'country':product.country,'image':product.image})
+        form = Products_upload_form(initial={'name':product.name,'price':product.price,'description':product.description,'country':product.country})
         context = {'form':form}
         return render(request, 'update_product.html', context=context)
 
