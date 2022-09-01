264673a5591c0c74f2b7122cbe10d8bab783c5d2
joseignaciotessio <109324425+joseignaciotessio@users.noreply.github.com>
2022-08-31 22:33:16 -0300

    Update urls.py
    correccion por merge

--- proyecto_final/proyecto_final/urls.py
+++ proyecto_final/proyecto_final/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from nutrisur.views import products, categories, containers,  index, sale, about \
-    , List_products, Create_product
+from nutrisur.views import create_products, products, categories, containers, create_categories, create_presentations, search_products, index, sale, about
 from django.conf.urls.static import static
 from django.conf import settings
-
+from nutrisur.views import delete_product, update_product,list_products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,11 +11,15 @@ urlpatterns = [
     path('', index, name='index'),
     path('Category/',categories, name = 'Category'),
     path('Container/', containers, name = 'Container'),
+    path('Products_upload_form/',create_products, name = 'Products_upload_form'),
+    path('Categories_upload_form/',create_categories, name = 'Categories_upload_form'),
+    path('create_presentaciones/',create_presentations, name = 'create_presentaciones'),
+    path('search_products/',search_products, name = 'search_products'),
     path('users/', include('users.urls')),
     path('sale/', sale, name ='sale'),
     path('about/', about, name ='about'),
-    path('list_products/', List_products.as_view(), name = 'list_products'),
-    path('create_product/', Create_product.as_view(), name = 'create_product'),
+    path('delete_product/<int:pk>/', delete_product, name='delete_product'),
+    path('update_product/<int:pk>/', update_product, name='update_product'),
+    path('list_products/', list_products, name='list_products'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
